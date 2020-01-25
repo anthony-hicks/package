31e1f99e1a3331dd4961fa96bd7745dc7947206e
Anthony Hicks <anthony-hicks@github.com>
2020-01-25 13:47:38 -0600

    Add nested scripts

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup
 setup(
     name='package',
     version='0.2',
-    scripts=['package-script']
+    scripts=[
+        'package-script',
+        'a/a-script',
+        'b/b-script'
+    ]
 )
